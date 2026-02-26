899a9b72a2acd4c5ec8c426c987b808e4cbe2dee
suryakanthgit <114014302+suryakanthgit@users.noreply.github.com>
2026-02-26 08:31:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def order():
         return "Error processing your order. Please try again.", 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0",port=5000)
