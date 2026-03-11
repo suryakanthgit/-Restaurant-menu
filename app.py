66123c2d2494cc52e061bf590e35e4c6a2d897c0
suryakanthgit <114014302+suryakanthgit@users.noreply.github.com>
2026-03-12 02:44:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
+        host=os.getenv("DB_HOST", "db"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", "password"),
+        database=os.getenv("DB_NAME", "restaurantdb"),
         port=int(os.getenv("DB_PORT", 3306))
     )
 
